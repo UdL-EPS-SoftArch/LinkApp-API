714b284614d2da9500c9c2e28b1fc50232180ef9
bcaspi <balcp11@hotmail.com>
2022-02-16 18:49:46 +0100

    Basic Message class created

--- src/main/java/cat/udl/eps/softarch/linkapp/domain/Message.java
+++ src/main/java/cat/udl/eps/softarch/linkapp/domain/Message.java
@@ -1,2 +1,45 @@
-package cat.udl.eps.softarch.linkapp.domain;public class Message {
+package cat.udl.eps.softarch.linkapp.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "Message")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Message extends UriEntity<String>  {
+
+    @Id
+    @GeneratedValue
+    Integer id;
+
+    @Temporal(TemporalType.DATE)
+    Date publicationDate;
+
+    @Temporal(TemporalType.TIME)
+    Date publicationTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    Date creationDateTime;
+
+    @NotBlank
+    @Column(unique = true)
+    private String text;
+
+    @NotBlank
+    @Column(unique = true)
+    private String author;
+
+    @NotBlank
+    @Column(unique = true)
+    private String meet;
+
+
+    public String getText() { return text; }
+    public String getAuthor() { return author; }
+    public String getMeet() { return meet; }
 }
