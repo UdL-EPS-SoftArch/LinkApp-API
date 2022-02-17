845386eb090d67b817a924460741b9f1c3a2f1cd
marcresaenrich <marcresaenrich@gmail.com>
2022-02-17 16:16:28 +0100

    Added Post repository

---
+++ src/main/java/cat/udl/eps/softarch/linkapp/repository/PostRepository.java
@@ -0,0 +1,22 @@
+package cat.udl.eps.softarch.linkapp.repository;
+
+import cat.udl.eps.softarch.linkapp.domain.Post;
+import cat.udl.eps.softarch.linkapp.domain.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface PostRepository extends PagingAndSortingRepository<Post, String> {
+
+  /* Interface provides automatically, as defined in https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/PagingAndSortingRepository.html
+   * count, delete, deleteAll, deleteById, existsById, findAll, findAllById, findById, save, saveAll
+   *
+   * Additional methods following the syntax defined in
+   * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+   */
+
+  List<Post> findByUsernameContaining(@Param("text") String text);
+}
