139a14496a6a5535c0652e5e15f078bda5699742
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-02-17 16:44:42 +0100

    feat handler dates pre hook

--- src/main/java/cat/udl/eps/softarch/linkapp/handler/MeetEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/linkapp/handler/MeetEventHandler.java
@@ -1,11 +1,14 @@
 package cat.udl.eps.softarch.linkapp.handler;
 
 import cat.udl.eps.softarch.linkapp.domain.Meet;
+import cat.udl.eps.softarch.linkapp.domain.User;
 import cat.udl.eps.softarch.linkapp.repository.MeetRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 @RepositoryEventHandler
@@ -20,7 +23,16 @@ public class MeetEventHandler {
     }
 
     @HandleBeforeCreate
-    public void handleMeetPreCreate(Meet meet) { logger.info("Before creating: {}", meet.toString()); }
+    public void handleMeetPreCreate(Meet meet) {
+        meet.setCreationDate(new Date());
+        logger.info("Before creating: {}", meet.toString());
+    }
+
+    @HandleBeforeSave
+    public void handleMeetPreSave(Meet meet) {
+        meet.setLastUpdate(new Date());
+        logger.info("Before updating: {}", meet.toString());
+    }
 
     @HandleAfterCreate
     public void handleMeetPostCreate(Meet meet) {
