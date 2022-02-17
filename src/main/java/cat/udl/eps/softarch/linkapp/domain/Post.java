89192963d8ce39da2fb29b8b45ab9cb6e2088ce6
marcresaenrich <marcresaenrich@gmail.com>
2022-02-17 16:02:41 +0100

    Created Post class

---
+++ src/main/java/cat/udl/eps/softarch/linkapp/domain/Post.java
@@ -0,0 +1,35 @@
+package cat.udl.eps.softarch.linkapp.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Post extends UriEntity<String>{
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotBlank
+	private String text;
+
+
+	@Override
+	public String getId() { return id.toString(); }
+
+}
