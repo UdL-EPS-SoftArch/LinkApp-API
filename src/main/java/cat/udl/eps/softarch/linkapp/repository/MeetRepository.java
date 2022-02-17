c9aa669d20f9a2a7ee1ede079711b25c298e0a1f
alexflorensa <florensa14@gmail.com>
2022-02-17 16:11:41 +0100

    Added Meet repository

---
+++ src/main/java/cat/udl/eps/softarch/linkapp/repository/MeetRepository.java
@@ -0,0 +1,17 @@
+package cat.udl.eps.softarch.linkapp.repository;
+
+import cat.udl.eps.softarch.linkapp.domain.Meet;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface MeetRepository extends PagingAndSortingRepository<Meet, String> {
+
+  /* Interface provides automatically, as defined in https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/PagingAndSortingRepository.html
+   * count, delete, deleteAll, deleteById, existsById, findAll, findAllById, findById, save, saveAll
+   *
+   * Additional methods following the syntax defined in
+   * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+   */
+
+}
