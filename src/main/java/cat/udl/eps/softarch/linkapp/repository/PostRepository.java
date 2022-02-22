b4ac90806e54ce43a34ea400b0d00d61ef59ae4d
xtrullols35 <59264503+xtrullols35@users.noreply.github.com>
2022-02-22 16:23:42 +0100

    Delete Not Used Imports PostRepository.java

--- src/main/java/cat/udl/eps/softarch/linkapp/repository/PostRepository.java
+++ src/main/java/cat/udl/eps/softarch/linkapp/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package cat.udl.eps.softarch.linkapp.repository;
 
 import cat.udl.eps.softarch.linkapp.domain.Post;
-import cat.udl.eps.softarch.linkapp.domain.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
