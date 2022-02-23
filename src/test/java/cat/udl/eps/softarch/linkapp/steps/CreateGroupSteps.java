02b04cf79f0908c55735cee154e9a08e77e99f15
Oriol Aguilarm <aguilars99@gmail.com>
2022-02-23 18:17:40 +0100

    Tests Group p1

--- src/test/java/cat/udl/eps/softarch/linkapp/steps/CreateGroupSteps.java
+++ src/test/java/cat/udl/eps/softarch/linkapp/steps/CreateGroupSteps.java
@@ -1,4 +1,24 @@
 package cat.udl.eps.softarch.linkapp.steps;
 
+import cat.udl.eps.softarch.linkapp.domain.Group;
+import cat.udl.eps.softarch.linkapp.repository.GroupRepository;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CreateGroupSteps {
+
+    @Autowired
+    private StepDefs stepDefs;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @When("^I Create a public Group called \"([^\"]*)\" ")
+    public void iCreateAPublicGroup(){
+        Group group = new Group(true);
+
+    }
+
+
 }
