cbdbc8682ab1035338034c48a450c7de7c102d58
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-02-23 20:56:16 +0100

    Composite Key Group Role and pre handling event

--- src/main/java/cat/udl/eps/softarch/linkapp/domain/GroupRole.java
+++ src/main/java/cat/udl/eps/softarch/linkapp/domain/GroupRole.java
@@ -11,27 +11,16 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Table(name="GroupRole", uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id", "group_id" }) })
 @Data
-public class GroupRole extends UriEntity<Long>{
+public class GroupRole extends UriEntity<GroupRoleKey>{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @ManyToOne(optional = false)
-    @JsonIdentityReference(alwaysAsId = true)
-    @JoinColumn(name = "group_id")
-    private Group group;
-
-    @ManyToOne(optional = false)
-    @JsonIdentityReference(alwaysAsId = true)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @EmbeddedId
+    private GroupRoleKey roleKey;
 
     @Enumerated(EnumType.STRING)
     private GroupRolesEnum role;
 
     @Override
-    public Long getId() {
-        return this.id;
+    public GroupRoleKey getId() {
+        return this.roleKey;
     }
 }
