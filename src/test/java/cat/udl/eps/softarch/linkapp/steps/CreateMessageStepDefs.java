6add9b880cab88cb9f0a550c07eec06213e57ebe
bcaspi <balcp11@hotmail.com>
2022-03-01 15:15:32 +0100

    Create Message StepDefs

--- src/test/java/cat/udl/eps/softarch/linkapp/steps/CreateMessageStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/linkapp/steps/CreateMessageStepDefs.java
@@ -1,16 +1,47 @@
 package cat.udl.eps.softarch.linkapp.steps;
 
+import cat.udl.eps.softarch.linkapp.domain.Group;
+import cat.udl.eps.softarch.linkapp.domain.Meet;
+import cat.udl.eps.softarch.linkapp.repository.GroupRepository;
+import cat.udl.eps.softarch.linkapp.repository.MeetRepository;
+import io.cucumber.java.en.And;
 import org.springframework.beans.factory.annotation.Autowired;
+
 
 public class CreateMessageStepDefs {
 
     @Autowired
     private StepDefs stepDefs;
 
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private MeetRepository meetRepository;
+
+    private Group group;
+
     CreateMessageStepDefs(StepDefs stepDefs) {
         this.stepDefs = stepDefs;
     }
 
-
+    @And("The meet with id {long} belongs to the group with id {long}")
+    public void theMeetWithIdExists(Long meetId, Long groupId) {
+        if (groupRepository.existsById(groupId)) groupRepository.deleteById(groupId);
+        Group group = new Group();
+        group.setId(groupId);
+        group.setTitle("title");
+        group.setDescription("description");
+        group.setVisibility(true);
+        groupRepository.save(group);
+        if (meetRepository.existsById(meetId)) meetRepository.deleteById(meetId);
+        Meet meet = new Meet();
+        meet.setId(meetId);
+        meet.setTitle("title");
+        meet.setGroup(group);
+        meet.setDescription("description");
+        meet.setStatus(true);
+        meetRepository.save(meet);
+    }
 
 }
--- src/test/resources/features/CreateMessage.feature
+++ src/test/resources/features/CreateMessage.feature
@@ -4,4 +4,15 @@ Feature: Create Message
 
   Background:
     Given There is a registered user with username "user" and password "password" and email "user@sample.app"
-    And I can login with username "user" and password "password"
+    And The group with id 1 exists
+    And The meet with id 1 belongs to the group with id 1
+
+  Scenario: Send a Message in a meet I am assisting
+  # TO-DO differences in user permissions when highlighting messages
+    Given I am authenticated as "user"
+    And The user "user" belongs to the group with id 1
+    # Meet class have similar tests --> TO-DO differences in user permissions when belonging to a group
+    And The user "user" is assisting to the meet with id 1
+    When I send a message in the meet with id 1 with message "message"
+    Then The response code is 201
+    # And It has been created a message with meet with (...)
