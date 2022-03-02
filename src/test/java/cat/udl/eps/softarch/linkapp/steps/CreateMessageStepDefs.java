c7c83384b35880b3dc366f78cef31137648bec03
bcaspi <balcp11@hotmail.com>
2022-03-02 18:02:51 +0100

    CreateMessage first Scenario passed

--- src/test/java/cat/udl/eps/softarch/linkapp/steps/CreateMessageStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/linkapp/steps/CreateMessageStepDefs.java
@@ -1,12 +1,20 @@
 package cat.udl.eps.softarch.linkapp.steps;
 
 import cat.udl.eps.softarch.linkapp.domain.*;
-import cat.udl.eps.softarch.linkapp.repository.GroupRepository;
-import cat.udl.eps.softarch.linkapp.repository.MeetRepository;
-import cat.udl.eps.softarch.linkapp.repository.UserRepository;
-import cat.udl.eps.softarch.linkapp.repository.UserRoleRepository;
+import cat.udl.eps.softarch.linkapp.repository.*;
+import com.jayway.jsonpath.JsonPath;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.time.ZonedDateTime;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
 public class CreateMessageStepDefs {
@@ -15,10 +23,10 @@ public class CreateMessageStepDefs {
     private StepDefs stepDefs;
 
     @Autowired
-    private GroupRepository groupRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    private MessageRepository messageRepository;
 
     @Autowired
     private UserRoleRepository userRoleRepository;
@@ -26,40 +34,76 @@ public class CreateMessageStepDefs {
     @Autowired
     private MeetRepository meetRepository;
 
-    private Group group;
+    @Autowired
+    private CreateMeetStepDefs meetStepDefs;
+
+    private static String username;
+    private static Group featureGroup;
+    private static Meet featureMeet;
+    private static Message featureMessage;
 
     CreateMessageStepDefs(StepDefs stepDefs) {
         this.stepDefs = stepDefs;
     }
 
-    @And("The meet with id {long} belongs to the group with id {long}")
-    public void theMeetWithIdExists(Long meetId, Long groupId) {
-        if (groupRepository.existsById(groupId)) groupRepository.deleteById(groupId);
-        Group group = new Group();
-        group.setId(groupId);
-        group.setTitle("title");
-        group.setDescription("description");
-        group.setVisibility(true);
-        groupRepository.save(group);
-        if (meetRepository.existsById(meetId)) meetRepository.deleteById(meetId);
+    @And("A group exists with a meet")
+    public void theMeetWithIdExists() {
+        featureGroup = meetStepDefs.theGroupExists();
         Meet meet = new Meet();
-        meet.setId(meetId);
         meet.setTitle("title");
-        meet.setGroup(group);
+        meet.setGroup(featureGroup);
         meet.setDescription("description");
         meet.setStatus(true);
+        meet.setLastUpdate(ZonedDateTime.now());
+        meet.setCreationDate(ZonedDateTime.now());
+        meet.setMeetDate(ZonedDateTime.now().plusDays(1));
+        featureMeet = meet;
         meetRepository.save(meet);
     }
 
-    @And("The user \"([^\"]*)\" is assisting to the meet with id {long}")
-    public void userIsAssistingToMeet(String username, Long meetId) {
+    @And("The user {string} is assisting to the meet")
+    public void userIsAssistingToMeet(String name) {
+        username = name;
         User user = userRepository.findById(username).get();
-        Meet meet = meetRepository.findById(meetId).get();
+        Meet meet = featureMeet;
+        Group group = featureGroup;
 
         UserRole userRole = userRoleRepository.
                 findByRoleKeyUserAndRoleKeyGroup(user, group);
 
         meet.getAttending().add(userRole);
+    }
+
+    @When("I send a message to the meet with message {string}")
+    public void sendMessageToMeet(String message) throws Throwable {
+        User user = userRepository.findById(username).get();
+        Message tmpMessage = new Message();
+        tmpMessage.setText(message);
+        tmpMessage.setGroup(featureGroup);
+        tmpMessage.setMeet(featureMeet);
+        tmpMessage.setAuthor(user);
+        tmpMessage.setCreationDate(ZonedDateTime.now());
+        stepDefs.result = stepDefs.mockMvc
+                .perform(
+                        post("/messages/")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .content(new JSONObject(stepDefs.mapper.writeValueAsString(tmpMessage))
+                                        .put("group", "/groups/" + featureGroup.getId())
+                                        .put("meet", "/meets/" + featureMeet.getId())
+                                        .toString()
+                                )
+                                .with(AuthenticationStepDefs.authenticate())
+                ).andDo(print());
+        MockHttpServletResponse response = stepDefs.result.andReturn().getResponse();
+        if (response.getStatus() == 201){
+            String content = stepDefs.result.andReturn().getResponse().getContentAsString();
+            String uri = JsonPath.read(content, "uri");
+            featureMessage = messageRepository.findById(Long.parseLong(uri.substring(uri.length() - 1))).get();
+        }
+        else if(response.getStatus() == 400){
+            // TO-DO
+        }
+
     }
 }
 
--- src/test/resources/features/CreateMessage.feature
+++ src/test/resources/features/CreateMessage.feature
@@ -4,26 +4,25 @@ Feature: Create Message
 
   Background:
     Given There is a registered user with username "user" and password "password" and email "user@sample.app"
-    And The group with id 1 exists
-    And The meet with id 1 belongs to the group with id 1
+    And A group exists with a meet
 
   Scenario: Send a Message in a meet I am assisting
   # TO-DO differences in user permissions when highlighting messages
-    Given I am authenticated as "user"
-    And The user "user" belongs to the group 1 as "AUTHORIZED"
+    Given I login as "user" with password "password"
+    And The user "user" belongs to that group as "AUTHORIZED"
     # Meet class have similar tests --> TO-DO differences in user permissions when belonging to a group
-    And The user "user" is assisting to the meet with id 1
-    When I send a message in the meet with id 1 with message "message"
+    And The user "user" is assisting to the meet
+    When I send a message to the meet with message "message"
     Then The response code is 201
     # And It has been created a message with meet with (...)
 
   Scenario: Send a blank message in a meet I am assisting
   # TO-DO differences in user permissions when highlighting messages
-    Given I am authenticated as "user"
-    And The user "user" belongs to the group 1 as "AUTHORIZED"
+    Given I login as "user" with password "password"
+    And The user "user" belongs to that group as "AUTHORIZED"
     # Meet class have similar tests --> TO-DO differences in user permissions when belonging to a group
-    And The user "user" is assisting to the meet with id 1
-    When I send a message in the meet with id 1 with message ""
+    And The user "user" is assisting to the meet
+    When I send a message to the meet with message ""
     Then The response code is 400
     And The error message is "must not be blank"
     # And It has been created a message with meet with (...)
