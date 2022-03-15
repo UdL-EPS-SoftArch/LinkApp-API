711caea39726baedea9e940c31750ed77edd5ede
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-03-15 16:02:00 +0100

    cleanup print in tests

--- src/test/java/cat/udl/eps/softarch/linkapp/steps/MeetStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/linkapp/steps/MeetStepDefs.java
@@ -155,12 +155,10 @@ public class MeetStepDefs
 
         JSONObject object = new JSONObject();
         object.put("role", UserRoleEnum.SUBSCRIBED);
-        String uri = userRole.getUri();
-        System.out.println(uri);
 
         // Patch updates one field whereas put overwrites all fields
         stepDefs.result = stepDefs.mockMvc.perform(
-                        patch(uri)
+                        patch(userRole.getUri())
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(object.toString())
                                 .accept(MediaType.APPLICATION_JSON)
